1f814ead1087401f6e1b0959d989008e59998c02
Patrick sang <christopherbii996@gmail.com>
2026-05-17 19:46:12 +0300

    Delete nairobi-flood-early-warning-system directory

--- nairobi-flood-early-warning-system/backend/deps.ts
+++
@@ -1,5 +0,0 @@
-export { Application, Router, send, Context } from 'oak';
-export { oakCors } from 'cors';
-export { config } from 'dotenv';
-export { format, differenceInHours, differenceInMinutes } from 'date-fns';
-export { getLogger, Logger } from 'std/log/mod.ts';
